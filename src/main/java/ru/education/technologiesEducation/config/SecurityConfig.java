69c0778bdb52900ebbb9d28fed3f7d12fcff1e92
Sergey <sersit444@mail.ru>
2022-12-09 17:47:40 +0400

    [V1Rest]. Update security service and database properties

--- src/main/java/ru/education/technologiesEducation/config/SecurityConfig.java
+++ src/main/java/ru/education/technologiesEducation/config/SecurityConfig.java
@@ -1,19 +1,32 @@
 package ru.education.technologiesEducation.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @EnableMethodSecurity
+@Configuration
 public class SecurityConfig {
     public static final String SIGN_UP_URL = "/api/v1/auth/registration";
     public static final String SIGN_IN_URL = "/api/v1/auth/login";
 
-
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http, UserDetailsService userDetailsService, BCryptPasswordEncoder passwordEncoder) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+        return authenticationManagerBuilder.build();
+    }
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http
--- src/main/java/ru/education/technologiesEducation/services/impl/UserDetailsServiceImpl.java
+++ src/main/java/ru/education/technologiesEducation/services/impl/UserDetailsServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.education.technologiesEducation.dao.UserRepository;
 import ru.education.technologiesEducation.model.Role;
@@ -13,7 +14,7 @@ import ru.education.technologiesEducation.model.User;
 
 import java.util.HashSet;
 import java.util.Set;
-
+@Service("userService")
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     private final UserRepository userRepository;
